b1226e74db7c97783afd82addf598b9d32bb2265
WirtaxYT <103338611+WirtaxYT@users.noreply.github.com>
2022-07-19 19:27:57 -0600

    Downgrade 1.8.1
    Downgrade 1.8.1

---
+++ Downgrade 1-8-1.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+    echo "Enter Maintenance Mode"
+    cd /var/www/pterodactyl
+    php artisan down
+
+    echo "Downgrade the Update"
+    rm panel.tar.gz
+    wget https://github.com/pterodactyl/panel/releases/download/v1.8.1/panel.tar.gz
+    chmod -R 755 storage/* bootstrap/cache
+
+    echo "Downgrade Dependencies"
+    composer install --no-dev --optimize-autoloader
+
+    echo "Clear Compiled Template Cache"
+    php artisan view:clear
+    php artisan config:clear
+
+    echo "Database Updates"
+    php artisan migrate --seed --force
+
+    echo "Set Permissions (DONT WORK ON CENTOS)"
+    chown -R www-data:www-data /var/www/pterodactyl/*
+    
+    echo "Restarting Queue Workers"
+    php artisan queue:restart
+
+    echo "Exit Maintenance Mode"
+    php artisan up
+
+    echo "Complete! Have a good day and dont forget to refresh your browser cache! (CTRL + F5)"
+    echo "-Will"
